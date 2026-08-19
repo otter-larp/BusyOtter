15a5a09da44d0a5f680027829f39187344f8dead
otter-larp <weltenjames@gmail.com>
2026-08-19 23:37:01 +0200

    Add files via upload
    BusyOtter RC2 is out!
    added:
    clear and info command

--- BusyOtter.bat
+++ BusyOtter.bat
@@ -19,6 +19,8 @@ if %cmd% == fetch goto fetch
 if %cmd% == help goto help
 if %cmd% == exit goto exit
 if %cmd% == ls goto ls
+if %cmd% == clear goto clear
+if %cmd% == info goto info
 goto cli
 :cli
 echo BusyOtter$
@@ -29,11 +31,11 @@ ipconfig
 ping google.com
 goto cli
 :ver
-echo BusyOtter version 0.0.1 RC1
+echo BusyOtter version 0.0.1 RC2
 goto cli
 :fetch
 echo BusyOtter version:
-echo BusyOtter 0.0.1 RC1
+echo BusyOtter 0.0.1 RC2
 echo windows version:
 ver
 echo language written in:
@@ -47,9 +49,19 @@ echo fetch *fetches system info*
 echo help *the command that shows this*
 echo ls *lists filesand directories in the directory busyotter is in*
 echo exit *terminates the BusyOtter script*
+echo clear *clears the screen*
+echo info *shows information about the lisence and github repo*
 goto cli
 :exit
 exit
 :ls
 dir
+goto cli
+:clear
+cls
+goto cli
+:info
+echo BusyOtter is lisenced under BSD-3 clause license
+echo github repository: https://github.com/otter-larp/BusyOtter/
+echo thanks for using/testing my project BusyOtter!
 goto cli
