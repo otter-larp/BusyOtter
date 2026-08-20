5c7a3daed87b0579e66962805fe61fda68819c7f
otter-larp <weltenjames@gmail.com>
2026-08-20 16:01:25 +0200

    Add files via upload
    sorry guys i accidently added 0.0.2 RC1 to main and not Release Candidate's

--- BusyOtter.bat
+++ BusyOtter.bat
@@ -7,12 +7,12 @@ pause
 cls
 goto welcome
 :welcome
-echo ---------
-echo BusyOtter
-echo ---------
+echo -----------
+echo ^|BusyOtter^|
+echo -----------
 goto cli
 :cmds
-set /p cmd=
+set /p "cmd=BusyOtter$ "
 if %cmd% == net goto net
 if %cmd% == ver goto ver
 if %cmd% == fetch goto fetch
@@ -21,9 +21,13 @@ if %cmd% == exit goto exit
 if %cmd% == ls goto ls
 if %cmd% == clear goto clear
 if %cmd% == info goto info
+if %cmd% == 67 goto 67
+if %cmd% == color-red goto red
+if %cmd% == color-blue goto blue
+if %cmd% == color-green goto green
+if %cmd% == color-reset goto reset
 goto cli
 :cli
-echo BusyOtter$
 goto cmds
 goto cli
 :net
@@ -31,11 +35,19 @@ ipconfig
 ping google.com
 goto cli
 :ver
-echo BusyOtter version 0.0.1 RC2
+echo BusyOtter version 0.0.2 RC1
 goto cli
 :fetch
+echo  ____                   ___  _   _
+echo ^| __ ) _   _ ___ _   _ / _ \^| ^|_^| ^|_ ___ _ __
+echo ^|  _ \^| ^| ^| / __^| ^| ^| ^| ^| ^| ^| __^| __/ _ \ '__^|
+echo ^| ^|_) ^| ^|_^| \__ \ ^|_^| ^| ^|_^| ^| ^|_^| ^|^|  __/ ^|
+echo ^|____/ \__,_^|___/\__, ^|\___/ \__^|\__\___^|_^|
+echo                  ^|___/
 echo BusyOtter version:
-echo BusyOtter 0.0.1 RC2
+echo BusyOtter 0.0.2 RC1
+echo user logged in:
+echo %USERNAME%
 echo windows version:
 ver
 echo language written in:
@@ -51,6 +63,7 @@ echo ls *lists filesand directories in the directory busyotter is in*
 echo exit *terminates the BusyOtter script*
 echo clear *clears the screen*
 echo info *shows information about the lisence and github repo*
+echo color- *you can add red or blue or green afther color to get those colors or reset to go to white*
 goto cli
 :exit
 exit
@@ -64,4 +77,19 @@ goto cli
 echo BusyOtter is lisenced under BSD-3 clause license
 echo github repository: https://github.com/otter-larp/BusyOtter/
 echo thanks for using/testing my project BusyOtter!
+goto cli
+:67
+echo go fuck yourself!
+exit
+:red
+color 4
+goto cli
+:blue
+color 1
+goto cli
+:green
+color 2
+goto cli
+:reset
+color 7
 goto cli
